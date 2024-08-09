abda82a4dd3d7b67e2ae70e889db68f3b717977d
Duy Nguyen <duy.nguyen@vn.defide.co.jp>
2024-08-09 17:40:33 +0700

    #add more config

---
+++ src/data/info.ts
@@ -0,0 +1,97 @@
+export const info = {
+  baseUrl: "https://bongoriilur.me",
+  name: "Bongoriilur",
+  jobDescription: "Designer",
+  about: `Tôi là một nhà thiết kế sống tại Việt Nam. Tôi đam mê tạo ra những trải nghiệm số sáng tạo và tương tác, từ giao diện trang web đến thiết kế ứng dụng và mọi thứ liên quan. Với 3 năm kinh nghiệm trong ngành thiết kế, tôi đã phát triển kỹ năng vững chắc trong việc xây dựng giao diện người dùng và trải nghiệm người dùng.
+
+Tôi thành thạo các công cụ thiết kế như Figma, Adobe XD, Sketch, và Photoshop, cũng như có kiến thức sâu về HTML, CSS, và JavaScript để hiện thực hóa các thiết kế thành sản phẩm thực tế. Nhờ sự kết hợp giữa thẩm mỹ thiết kế và kỹ thuật, tôi đã hoàn thành nhiều dự án web và ứng dụng từ ý tưởng đến sản phẩm cuối cùng.
+
+Tôi luôn không ngừng học hỏi và cập nhật các xu hướng mới trong thiết kế, nhằm tạo ra những sản phẩm có giá trị cao và trải nghiệm người dùng xuất sắc.
+
+  `,
+
+  "experience": [
+    {
+      "name": "InterNative Labs, LLC",
+      "location": "Frog Design, Việt Nam",
+      "startDate": "Apr 2022",
+      "endDate": "Present",
+      "description": [
+        "- Thiết kế và phát triển giao diện người dùng cho ứng dụng web sử dụng React và Tailwind CSS.",
+        "- Sửa lỗi giao diện và tích hợp API từ backend thông qua Redux Saga."
+      ]
+    },
+    {
+      "name": "Coupon Approve (Remote)",
+      "location": "IDEO, San Francisco, USA",
+      "startDate": "Nov 2020",
+      "endDate": "Apr 2022",
+      "description": [
+        "✓ Phát triển mã nguồn theo các quy tắc thiết kế và kiến trúc phù hợp.",
+        "✓ Tài liệu hóa, kiểm thử, triển khai, bảo trì và hỗ trợ các nền tảng, ứng dụng, cơ sở dữ liệu và giải pháp.",
+        "✓ Hỗ trợ và đào tạo các đồng nghiệp khác.",
+        "✓ Thực hiện các nhiệm vụ khác liên quan đến công việc.",
+        "✓ Cải tiến mã và tái cấu trúc.",
+        "✓ Duy trì các thực hành tốt nhất và quy tắc trong phát triển."
+      ]
+    },
+    {
+      "name": "Interns Pakistan",
+      "location": "Pentagram, New York, USA",
+      "startDate": "Oct 2020",
+      "endDate": "Nov 2020",
+      "description": [
+        "Một chương trình thực tập chuyên sâu, giúp tôi chuẩn bị cho những công việc được yêu cầu cao nhất trên thế giới. Chương trình kéo dài sáu tuần tập trung vào học tập và thử thách.",
+        "Chương trình thực tập được xây dựng bởi các chuyên gia trong ngành, giúp phát triển các kỹ năng trong các lĩnh vực quan trọng nhất."
+      ]
+    }
+  ],
+
+  "education": [
+    {
+      "name": "Đại học Kiến trúc TP.HCM",
+      "location": "196 Pasteur, Phường 6, Quận 3, TP.HCM, Việt Nam",
+      "startDate": "2019",
+      "endDate": "2022",
+      "description": [
+        "Diploma of Associate Engineering, Computer Information Technology (CIT)"
+      ]
+    },
+    {
+      "name": "Arena Multimedia",
+      "location": "212-214 Nguyễn Đình Chiểu, Phường 6, Quận 3, TP.HCM, Việt Nam",
+      "startDate": "2018",
+      "endDate": "2020",
+      "description": [
+        "Intermediate, Pre-Engineering"
+      ]
+    },
+    {
+      "name": "Đồ họa Thùy Uyên",
+      "location": "54 Lê Thị Hồng Gấm, Phường Nguyễn Thái Bình, Quận 1, TP.HCM, Việt Nam",
+      "startDate": "2016",
+      "endDate": "2018",
+      "description": [
+        "Matriculation, Computer Science"
+      ]
+    }
+  ],
+
+  socialMedia: {
+    facebook: "https://www.instagram.com/bongoriilur/",
+    twitter: "https://www.instagram.com/bongoriilur/",
+    github: "https://www.instagram.com/bongoriilur/",
+    email: "mailto:bongoriilur@gmail.com",
+    linkedin: "https://www.instagram.com/bongoriilur/",
+  },
+
+  projects: [
+    {
+      title: "Bongoriilur Portfolio",
+      isFeatured: true,
+      thumbnail: "/assets/images/astro-portfolio.png",
+      githubUrl: "https://www.instagram.com/bongoriilur/",
+      liveUrl: "https://bongoriilur.me",
+    },
+  ],
+};
